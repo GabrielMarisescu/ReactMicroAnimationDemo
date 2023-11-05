ea3cd1fcfe55787556adfda5849213ade30b65fe
GabrielMarisescu <gabriel.lavoro10@gmail.com>
2023-11-05 18:10:53 +0100

    testCommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import React, { ReactElement, useEffect, useRef, useState } from 'react';
-import ReactDOM from 'react-dom';
-import Tween from './animations/Tween';
-import Spring from './animations/Spring';
-import './index.scss';
-import { Refresh } from './components/Refresh';
-import ShowAnimations from './components/ShowAnimations';
-import Repeat_Infinite from './animations/Repeat_Infinite';
-import Repeat_Backwards from './animations/Repeat_Backwards';
-import While_Hover from './animations/While_Hover';
-import Presentation from './components/Presentation';
-import While_Tap from './animations/While_Tap';
-import While_Drag from './animations/While_Drag';
-import Cycle_States from './animations/Cycle_States';
-import Animation_Sequence from './animations/Animation_Sequence';
+import React, { ReactElement, useEffect, useRef, useState } from "react";
+import ReactDOM from "react-dom";
+import Tween from "./animations/Tween";
+import Spring from "./animations/Spring";
+import "./index.scss";
+import { Refresh } from "./components/Refresh";
+import ShowAnimations from "./components/ShowAnimations";
+import Repeat_Infinite from "./animations/Repeat_Infinite";
+import Repeat_Backwards from "./animations/Repeat_Backwards";
+import While_Hover from "./animations/While_Hover";
+import Presentation from "./components/Presentation";
+import While_Tap from "./animations/While_Tap";
+import While_Drag from "./animations/While_Drag";
+import Cycle_States from "./animations/Cycle_States";
+import Animation_Sequence from "./animations/Animation_Sequence";
 
 const animationComponents = {
-  'Tween Animation': Tween,
-  'Spring animation': Spring,
-  'Repeat Infinite Animation': Repeat_Infinite,
-  'Repeat Backwards Animation': Repeat_Backwards,
-  'While Hover Animation': While_Hover,
-  'While Tap Animation': While_Tap,
-  'While Drag Animation': While_Drag,
-  'Cycle States Animation': Cycle_States,
-  'Animation Sequence ': Animation_Sequence,
+  "Tween Animation": Tween,
+  "Spring animation": Spring,
+  "Repeat Infinite Animation": Repeat_Infinite,
+  "Repeat Backwards Animation": Repeat_Backwards,
+  "While Hover Animation": While_Hover,
+  "While Tap Animation": While_Tap,
+  "While Drag Animation": While_Drag,
+  "Cycle States Animation": Cycle_States,
+  "Animation Sequence ": Animation_Sequence,
 };
 
 const animationsComponentsArr = Object.entries(animationComponents);
@@ -31,25 +31,29 @@ const animationsComponentsArr = Object.entries(animationComponents);
 function App() {
   const [count, setCount] = useState(0);
   const [animationCount, setAnimationCount] = useState(0);
+
+  const test = [{ id: "test3" }, { id: "test2" }, { id: "test" }];
+
   const [currentAnimationName, setCurrentAnimationName] = useState<string>(
-    animationsComponentsArr[0]?.[0]
+    animationsComponentsArr[0]?.[0],
   );
 
   const forwardAnimation = (): void => {
     setAnimationCount((prevData) =>
-      prevData + 1 > animationsComponentsArr.length - 1 ? 0 : prevData + 1
+      prevData + 1 > animationsComponentsArr.length - 1 ? 0 : prevData + 1,
     );
   };
 
   const backwardAnimation = (): void => {
+    console.log("gabriel");
     setAnimationCount((prevData) =>
-      prevData - 1 < 0 ? animationsComponentsArr.length - 1 : prevData - 1
+      prevData - 1 < 0 ? animationsComponentsArr.length - 1 : prevData - 1,
     );
   };
   const showAnimations = () => {
     let index = Math.min(
       Math.max(animationCount, 0),
-      animationsComponentsArr.length - 1
+      animationsComponentsArr.length - 1,
     );
 
     const item = animationsComponentsArr?.[index];
@@ -64,8 +68,8 @@ function App() {
 
   return (
     <>
-      <Presentation />
-      <div className='flex justify-center mt-10 text-2xl font-semibold'>
+      /* <Presentation /> */
+      <div className="flex justify-center mt-10 text-2xl font-semibold">
         {animationCount + 1} {currentAnimationName}
       </div>
       <Refresh onClick={() => setCount(count + 1)} />
@@ -79,4 +83,4 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
